2b82bd23be074501428739e62cf88907f698075a
Nayandeep Shrestha <58981963+nayandeep-shrestha@users.noreply.github.com>
2022-11-22 22:45:59 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import Loader from './components/loader/Loader'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
-import Contact from './components/contact/Contact'
+// import Contact from './components/contact/Contact'
 import Portfolio from './components/portfolio/Portfolio'
 import Home from './components/home/Home'
 import Skills from './components/skills/Skills'
