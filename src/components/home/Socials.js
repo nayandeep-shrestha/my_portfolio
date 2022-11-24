3f2ff7b041eb8ac5a568e4fca7b9cef2d5cf7881
Nayandeep Shrestha <shresnayan@gmail.com>
2022-11-24 19:17:50 +0545

    nav social css

--- src/components/home/Socials.js
+++ src/components/home/Socials.js
@@ -8,10 +8,10 @@ import {BsInstagram} from 'react-icons/bs'
 const Socials = () => {
   return (
     <div className='header-social'>
-        <a href="https://github.com/nayandeep-shrestha" target="_blank" rel="noreferrer"><BsGithub/></a>
-        <a href="https://www.linkedin.com/in/nayandeep-shrestha-a9a08a23a/" target="_blank" rel="noreferrer"><BsLinkedin/></a>
-        <a href="https://twitter.com/_nayandeep_" target="_blank" rel="noreferrer"><BsTwitter/></a>
-        <a href="https://www.instagram.com/nayandeep_shrestha/" target="_blank" rel="noreferrer"><BsInstagram/></a>
+        <a href="https://github.com/nayandeep-shrestha" target="_blank" rel="noreferrer" className='social-icon'><BsGithub/></a>
+        <a href="https://www.linkedin.com/in/nayandeep-shrestha-a9a08a23a/" target="_blank" rel="noreferrer" className='social-icon'><BsLinkedin/></a>
+        <a href="https://twitter.com/_nayandeep_" target="_blank" rel="noreferrer" className='social-icon'><BsTwitter/></a>
+        <a href="https://www.instagram.com/nayandeep_shrestha/" target="_blank" rel="noreferrer" className='social-icon'><BsInstagram/></a>
     </div>
   )
 }
